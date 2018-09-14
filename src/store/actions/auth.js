ee2048bc614929229c36666b7d8ed4cd1b0135ef
Paulo Loaeza <luis.loaezagar@gmail.com>
2018-09-13 20:15:10 -0500

    guards added

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -16,14 +16,38 @@ export const authFail = (error) => ({
     error
 });
 
-export const logout = () => ({
-    type: actionsTypes.AUTH_LOGOUT
-});
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
+    return {
+        type: actionsTypes.AUTH_LOGOUT
+    };
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expDate = new Date(localStorage.getItem('expirationDate'));
+            if (expDate <= new Date()) {
+                dispatch(logout());
+            } else {
+                const userId = localStorage.getItem('userId');
+                const expTime = (expDate.getTime() - new Date().getTime()) / 1000;
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(expTime));
+            }
+        }
+    }
+}
 
 export const checkAuthTimeout = (expTime) => {
     return dispatch => {
         setTimeout(() => {
-            dispatch(logout())
+            dispatch(logout());
         }, expTime * 1000)
     }
 }
@@ -45,6 +69,10 @@ export const auth = (email, password, isSignUp) => {
         axios
             .post(url, auth)
             .then(res => {
+                const expDate = new Date(new Date().getTime() + res.data.expiresIn * 1000);
+                localStorage.setItem('token', res.data.idToken);
+                localStorage.setItem('expirationDate', expDate);
+                localStorage.setItem('userId', res.data.localId);
                 dispatch(authSuccess(res.data.idToken, res.data.localId));
                 dispatch(checkAuthTimeout(res.data.expiresIn))
             })
